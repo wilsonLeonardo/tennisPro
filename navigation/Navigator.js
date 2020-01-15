b98701d4b57798a9c4ad509e3d45a130ab750bbb
Renan Oliveira <renan04lima@outlook.com>
2020-01-15 17:50:40 -0300

    adicionado tela Tipo de Conta

--- navigation/Navigator.js
+++ navigation/Navigator.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from '../screens/HomeScreen'
+import TipoDeConta from '../screens/TipoDeConta'
 
 
 const AppNavigator = createStackNavigator({
-  Home: {
-    screen: HomeScreen,
-  },
+  // Home: {
+  //   screen: HomeScreen,
+  // },
+
+  Conta: {
+    screen: TipoDeConta,
+  }
+
 });
 
 export default createAppContainer(AppNavigator);
