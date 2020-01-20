d62da225b7a539235387b98d083bcbbfafe8f222
Renan Oliveira <renan04lima@outlook.com>
2020-01-19 23:44:47 -0300

    adicionado tela Cadastro Professor: Disponibilidade

--- navigation/Navigator.js
+++ navigation/Navigator.js
@@ -9,6 +9,7 @@ import NivelAtletaProf from '../screens/NivelAtletaProf'
 import ClubDataScreen from '../screens/Club/Register/ClubDataScreen'
 import ClubDiponibilidade from '../screens/Club/Register/ClubDiponibilidade'
 import TeacherDataScreen from '../screens/Teacher/Register/TeacherDataScreen';
+import TeacherDiponibilidade from '../screens/Teacher/Register/TeacherDiponibilidade';
 
 const AppNavigator = createStackNavigator({
   Home: {
@@ -35,6 +36,9 @@ const AppNavigator = createStackNavigator({
   clubeDispo: {
     screen: ClubDiponibilidade
   },
+  teacherDispo:{
+    screen: TeacherDiponibilidade
+  }
 
 });
 
