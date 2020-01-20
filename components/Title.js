0bb5f293020169f1d65d69be463eddb6779ea946
Wilson Leonardo <wilsonleonardo1@gmail.com>
2020-01-20 02:18:15 -0300

    Update: Restruturação em components e nova design do projeto

---
+++ components/Title.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { View, StyleSheet, Image } from 'react-native';
+import {AeroText} from './StyledText'
+import * as Icon from '../components/Icon/Icon'
+
+export function TitleTennis(props) {
+  return (
+    <View style={styles.title}>
+        <View style={styles.caixa} >
+            {props.Icon == 'Check' ? <Icon.CheckIcon/> : props.Icon == 'Person' ? <Icon.PersonIcon/> :
+             props.Icon == 'Ball' ? <Icon.Ball2Icon/> : props.Icon == 'Star' ? <Icon.StarIcon/> : null}
+            <AeroText style={[{ fontSize: 18, left:10, alignItems: 'center', color: '#f75400' },props.style]}>
+                {`${props.placeholder}`}
+            </AeroText>
+        </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    title: {
+        backgroundColor:'#fff',
+        alignItems:'center',
+        justifyContent:'center',
+    },
+    caixa: {
+        flexDirection:'row',
+        width: 300,
+        height: 50,
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+});
