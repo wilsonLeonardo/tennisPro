5f774235241253b6a9119faa56c7b4ba5df72803
Wilson <wilsonleonardo@MacBook-Air-do-Wilson.local>
2020-04-26 20:22:59 -0300

    fix: message item

--- app/screens/User/App/components/MessageItem.js
+++ app/screens/User/App/components/MessageItem.js
@@ -11,9 +11,6 @@ const boxWidth = Dimensions.get('window').width - 220;
 
 
 export default class MessageItem extends React.Component {
-
-     
-
     render() {
 
         const { message} = this.props;
